86a41ce3428f04fe953e5ee76738298a4351ab5e
teinejo <80319351+teinejo@users.noreply.github.com>
2022-02-18 00:53:32 +0300

    rework getter & setter

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,7 +1,13 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+@Getter
+@Setter
+@Data
 public class Student {
     private Long id;
     private String name;
@@ -28,46 +34,6 @@ public class Student {
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
         this.age = age;
     }
 
