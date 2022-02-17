2df2fda2a983404adcd0ef17a67c77da17645441
teinejo <80319351+teinejo@users.noreply.github.com>
2022-02-18 00:22:52 +0300

    First commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,30 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 20),
+                        5
+                ),
+                new Student(
+                        2L,
+                        "Anna",
+                        "bio@gmail.ru",
+                        LocalDate.of(2001, Month.APRIL, 28),
+                        10
+                )
+        );
+    }
+}
